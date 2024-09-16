2afc16cc8890a090aea859be5ea63f58233877a8
root <root@DESKTOP-HDVOUFV>
2024-09-16 23:06:44 +0300

    Added Apple Integer Basic with wozmon. Added emulation of RAM and ROM memory areas. Various improvements to test main.

--- 6502/main.c
+++ 6502/main.c
@@ -19,7 +19,7 @@
 #define SCREEN_H 25
 #define REFRESH_RATE_NS MS_TO_NS(20)
 #define KB_SCAN_RATE_NS	MS_TO_NS(10)
-#define TICK_NS (time_t)((double)1 / (double)CLOCK_FREQ * (double)S_TO_NS(1)) //1000
+#define TICK_NS (time_t)((double)1 / (double)CLOCK_FREQ * (double)S_TO_NS(1))
 
 #define TSPEC_NSEC_MAX 999999999
 
@@ -41,7 +41,7 @@ enum {
 	PRG_BLINK,
 	PRG_NUMQUERY,
 	PRG_PRINT,
-	PRG_WOZMON,
+	PRG_APPLE_I,
 	PRG_OPCODETEST
 } programList;
 
@@ -51,14 +51,19 @@ typedef enum {
 	mos6502_ASM
 } mos6502_fileType;
 
+typedef struct {
+	mos6502_addr startAddr;
+	mos6502_addr endAddr;
+} address_range;
+
 mos6502_addr keyboardAddr = 0xFDFF;
 mos6502_addr screenMemAddr = 0xF800;
 mos6502_addr screenMemSize = SCREEN_W * SCREEN_H;
 
-void loadFile(U8 * pMemory, U16 offset, const char * pPath, mos6502_fileType fileType);
+void loadFile(U8 * pMemory, mos6502_addr offset, const char * pPath, mos6502_fileType fileType);
 
-U8 fnMemRead(U16 address);
-void fnMemWrite(U16 address, U8 value);
+U8 fnMemRead(mos6502_addr address);
+void fnMemWrite(mos6502_addr address, U8 value);
 
 U8 readKeyboard(void);
 
@@ -68,16 +73,31 @@ void terminalOutput(void);
 
 void programSelector(int selected_program, U8 * pMemory);
 void programEnd(int selected_program, U8 * pMemory, mos6502_processor_st * pProcessor);
+void addROMArea(mos6502_addr startAddr, mos6502_addr endAddr);
+U8 isROMAddress(mos6502_addr address);
+void addRAMArea(mos6502_addr startAddr, mos6502_addr endAddr);
+U8 isRAMAddress(mos6502_addr address);
 
 U8 timePassed(struct timespec * oldTime, struct timespec * newTime, time_t timeToPass);
 
 U8 appleKeyConv(U8 input);
+U8 appleScreenConv(U8 input);
 
 void logTrace(void);
 
 U8 * ram;
 
-U8 cursorX, cursorY;
+char lineBuf[512];
+int lineBufIndex = 0;
+char inBuff[20];
+int inBuffIndex = 0;
+
+int * g_selectedProgram;
+
+U8 ROMAddrRangeCount;
+address_range ROMAddrList[0xFF];
+U8 RAMAddrRangeCount;
+address_range RAMAddrList[0xFF];
 
 extern opCode_st * mos6502__opCodes;
 
@@ -85,12 +105,6 @@ extern char * traceBuf[80];
 extern U8 traceBufIndex;
 
 extern FILE * tmpLog;
-char lineBuf[512];
-int lineBufIndex = 0;
-char inBuff[20];
-int inBuffIndex = 0;
-
-int * g_selectedProgram;
 
 /*******************************************************************************
 * Handles loading and running the program.
@@ -166,7 +180,7 @@ int main(int argc, char * argv[]) {
 		printf("\t%d - Cursor blink\n", PRG_BLINK);
 		printf("\t%d - Number query\n", PRG_NUMQUERY);
 		printf("\t%d - String print test\n", PRG_PRINT);
-		printf("\t%d - Wozmon\n", PRG_WOZMON);
+		printf("\t%d - Apple I emulator\n", PRG_APPLE_I);
 		printf("\t%d - Opcode test\n", PRG_OPCODETEST);
 		printf("\n\n");
 		printf("Options:\n\t-d : Print operations.\n\t-s(f/n) : Slow down 1-1000 ms/op (def 500)\n\t          f: 1-1000 us/op\n\t          n: no limit\n\n");
@@ -237,7 +251,7 @@ int main(int argc, char * argv[]) {
 		if (timePassed(&refreshTime, &runTime, REFRESH_RATE_NS)) {
 			refreshTime = runTime;
 			
-			if (selected_program == PRG_WOZMON) {
+			if (selected_program == PRG_APPLE_I) {
 				DRAW_TERM(terminalOutput());
 			} else {
 				DRAW_TERM(screenRefresh(frequency));
@@ -250,21 +264,38 @@ int main(int argc, char * argv[]) {
 			kbScanTime = runTime;
 
 			pressedKey = readKeyboard();
+			if (pressedKey == 0x1B) { // ESC detected.
+				printf("\b \b");
+				pressedKey = readKeyboard();
 
-			if (pressedKey == 0x1B) {
-				system("/bin/stty cooked");
-				printf("%s%s", CURSOR_RESET, TERM_RESET);
+				if (pressedKey == 0xFF || pressedKey == 0x1B) {  // ESC key pressed, quit.
+					system("/bin/stty cooked");
+					printf("%s%s", CURSOR_RESET, TERM_RESET);
 
-				printf("ESC key hit.\n");
-				exit(0);
+					printf("ESC key hit.\n");
+
+					logTrace();
+					exit(0);
+				} else if (pressedKey == '[') { // CTRL character detected.
+					printf("\b \b");
+					pressedKey = readKeyboard();
+					printf("\b \b");
+					pressedKey = readKeyboard();
+					printf("\b \b");
+				} else { // Unknown ESC sequence.
+					printf("\b \b");
+				}
+			} else if (pressedKey == 0x09) { // TAB pressed (reset).
+				mos6502_init(&processor, fnMemRead, fnMemWrite, debugLogging);
+				printf("\b \b\b \b\b \b\b \b\b \b\b \b\b \b");
 			}
-
-			if (selected_program == PRG_WOZMON) {
+			
+			if (selected_program == PRG_APPLE_I) {
 
 				pressedKey = appleKeyConv(pressedKey);
 
 				if (pressedKey != 0xFF
-				&&	!(ram[0xD011] & 0x80)
+				&&	!(fnMemRead(0xD011) & 0x80)
 				) {
 
 					inBuff[inBuffIndex++] = pressedKey;
@@ -273,19 +304,19 @@ int main(int argc, char * argv[]) {
 						fprintf(tmpLog, "INPUT: %s\r\n", inBuff);
 						inBuffIndex = 0;
 					}
-					ram[0xD011] = 0x80;
-					ram[0xD010] = pressedKey + 0x80;
+					fnMemWrite(0xD011, 0x80);
+					fnMemWrite(0xD010, pressedKey + 0x80);
 
 				} else if (
 					pressedKey == 0xFF
-				&&	ram[0xD011] & 0x80
+				&&	fnMemRead(0xD011) & 0x80
 				) {
-					ram[0xD011] = 0;
+					fnMemWrite(0xD011, 0);
 
 				} else {
 				}
 			} else {
-				ram[keyboardAddr] = pressedKey;
+				fnMemWrite(keyboardAddr, pressedKey);
 			}
 		}
 
@@ -384,9 +415,12 @@ void loadFile(U8 * pMemory, U16 offset, const char * pPath, mos6502_fileType fil
 * Returns: Byte read from the memory.
 *******************************************************************************/
 U8 fnMemRead(mos6502_addr address) {
-	//printf("MEM READ: $%04X: $%02X\r\n", address, ram[address]);
-	if (*g_selectedProgram == PRG_WOZMON && address == 0xD010) {
+	if (*g_selectedProgram == PRG_APPLE_I && address == 0xD010) {
 		ram[0xD011] = 0;
+	}
+	
+	if (!isROMAddress(address) && !isRAMAddress(address)) {
+		return 0;
 	}
 
 	return ram[address];
@@ -402,7 +436,7 @@ U8 fnMemRead(mos6502_addr address) {
 * Returns: Nothing.
 *******************************************************************************/
 void fnMemWrite(mos6502_addr address, U8 value) {
-	if (address != keyboardAddr) {
+	if (!isROMAddress(address)) {
 		ram[address] = value;
 	}
 }
@@ -441,12 +475,12 @@ U8 readKeyboard(void) {
 void screenClear(void) {
 	system("clear");
 
-	if (*g_selectedProgram == PRG_WOZMON) return;
+	if (*g_selectedProgram == PRG_APPLE_I) return;
 
 	printf("%s%s", CURSOR_OFF, COLOR_GRY_BLK);
 
 	for (int memIndex = screenMemAddr + screenMemSize; memIndex >= screenMemAddr; memIndex--) {
-		ram[memIndex] = ' ';
+		fnMemWrite(memIndex, ' ');
 	}
 }
 
@@ -493,22 +527,27 @@ void screenRefresh(unsigned int frequency) {
 *******************************************************************************/
 void terminalOutput(void) {
 
-	if (ram[0xD012] != 0) {
-		if (ram[0xD012] & 0x80) {
-			U8 chr = ram[0xD012] & ~0x80;
+	U8 memChar = fnMemRead(0xD012);
 
-			lineBuf[lineBufIndex++] = chr;
+	if (memChar != 0) {
+		if (memChar & 0x80) {
+			U8 chr = appleScreenConv(memChar & ~0x80);
 
-			if (chr == '\r') {
-				lineBuf[lineBufIndex - 1] = '\0';
-				fprintf(tmpLog, "OUTPUT: %s\r\n", lineBuf);
-				lineBufIndex = 0;
+			if (chr != 0xFF) {
+				lineBuf[lineBufIndex++] = chr;
+
+				if (chr == '\r') {
+					lineBuf[lineBufIndex - 1] = '\0';
+					fprintf(tmpLog, "OUTPUT: %s\r\n", lineBuf);
+					lineBufIndex = 0;
+				}
+
+				printf("%c%s", chr, chr == '\r' ? "\n" : "");
 			}
-
-			printf("%c%s", chr, chr == '\r' ? "\n" : "");
 		}
 	}
-	ram[0xD012] = 0;
+
+	fnMemWrite(0xD012, 0);
 }
 
 /*******************************************************************************
@@ -526,24 +565,40 @@ void programSelector(int selected_program, U8 * pMemory) {
 		exit(0);
 	} else if (selected_program == PRG_STORE_TO_MEM) { // write values to memory.
 		loadFile(pMemory, 0xFE00, "./prg/bin/memWrite.6502", mos6502_BIN);
+		addRAMArea(0x0000, 0xFFFF); // System & User space
 	} else if (selected_program == PRG_CLEAR_MEM) { // Clears X amount of memory from memory address (100), Y
 		loadFile(pMemory, 0xFE00, "./prg/bin/memClear.6502", mos6502_BIN);
+		addRAMArea(0x0000, 0xFFFF); // System & User space
 	} else if (selected_program == PRG_WEEKDAY) { // Day of the week. Y = year, X = month, AC = day
 		loadFile(pMemory, 0xFE00, "./prg/bin/weekday.6502", mos6502_BIN);
+		addRAMArea(0x0000, 0xFFFF); // System & User space
 	} else if (selected_program == PRG_KEYBOARD) {
-		loadFile(pMemory, 0xFE00, "./prg/bin/kbTest.6502", mos6502_BIN);		
+		loadFile(pMemory, 0xFE00, "./prg/bin/kbTest.6502", mos6502_BIN);
+		addRAMArea(0x0000, 0xFFFF); // System & User space
 	} else if (selected_program == PRG_BLINK) {
 		loadFile(pMemory, 0xFE00, "./prg/bin/blink.6502", mos6502_BIN);
+		addRAMArea(0x0000, 0xFFFF); // System & User space
 	} else if (selected_program == PRG_NUMQUERY) {
 		loadFile(pMemory, 0xFE00, "./prg/bin/numQuery.6502", mos6502_BIN);
+		addRAMArea(0x0000, 0xFFFF); // System & User space
 	} else if (selected_program == PRG_PRINT) {
 		loadFile(pMemory, 0xFE00, "./prg/bin/print.6502", mos6502_BIN);
-	} else if (selected_program == PRG_WOZMON) {
-		loadFile(pMemory, 0xFF00, "./prg/bin/wozmon.6502", mos6502_BIN);
+		addRAMArea(0x0000, 0xFFFF); // System & User space
+	} else if (selected_program == PRG_APPLE_I) {
+		loadFile(pMemory, 0xE000, "./prg/apple_i/bin/intBasic.6502", mos6502_BIN);
+		loadFile(pMemory, 0xFF00, "./prg/apple_i/bin/wozmon.6502", mos6502_BIN);
 		screenMemAddr = 0xD012;
 		keyboardAddr = 0xD010;
+		addRAMArea(0x0000, 0x7FFF); // System & User space
+		addRAMArea(0xD010, 0xD010); // Keyboard In
+		addRAMArea(0xD011, 0xD011); // Keyboard Control
+		addRAMArea(0xD012, 0xD012); // Display Out
+		addRAMArea(0xD013, 0xD013); // Display Control
+		addROMArea(0xE000, 0xEFFF); // Integer Basic
+		addROMArea(0xFF00, 0xFFFF); // Monitor
 	} else if (selected_program == PRG_OPCODETEST) {
 		loadFile(pMemory, 0x0000, "./prg/test/6502_functional_test.bin", mos6502_BIN);
+		addRAMArea(0x0000, 0xFFFF); // System & User space
 	}
 }
 
@@ -627,6 +682,17 @@ U8 appleKeyConv(U8 input) {
 	return input;
 }
 
+U8 appleScreenConv(U8 input) {
+
+	if (input == '\r' || (input >= ' ' && input <= '_')) {
+		// Capital letters, numbers, return and some special characters are passed through
+	} else {
+		input = 0xFF;
+	}
+
+	return input;
+}
+
 
 void logTrace(void) {
 	U8 tempIndex;
@@ -653,4 +719,50 @@ void logTrace(void) {
 			}
 		}
 	}
+}
+
+void addROMArea(mos6502_addr startAddr, mos6502_addr endAddr) {
+	if (startAddr > endAddr) {
+		printf("\n\nInvalid ROM area range (0x%04x - 0x%04x)...\n\n", startAddr, endAddr);
+		exit(1);
+	}
+
+	ROMAddrList[ROMAddrRangeCount].startAddr = startAddr;
+	ROMAddrList[ROMAddrRangeCount].endAddr = endAddr;
+
+	ROMAddrRangeCount++;
+}
+U8 isROMAddress(mos6502_addr address) {
+	for (int areaIdx = 0; areaIdx < ROMAddrRangeCount; areaIdx++) {
+		if (ROMAddrList[areaIdx].startAddr <= address
+		&&	ROMAddrList[areaIdx].endAddr >= address
+		) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+void addRAMArea(mos6502_addr startAddr, mos6502_addr endAddr) {
+	if (startAddr > endAddr) {
+		printf("\n\nInvalid RAM area range (0x%04x - 0x%04x)...\n\n", startAddr, endAddr);
+		exit(1);
+	}
+
+	RAMAddrList[RAMAddrRangeCount].startAddr = startAddr;
+	RAMAddrList[RAMAddrRangeCount].endAddr = endAddr;
+
+	RAMAddrRangeCount++;
+}
+U8 isRAMAddress(mos6502_addr address) {
+	for (int areaIdx = 0; areaIdx < RAMAddrRangeCount; areaIdx++) {
+		if (RAMAddrList[areaIdx].startAddr <= address
+		&&	RAMAddrList[areaIdx].endAddr >= address
+		) {
+			return 1;
+		}
+	}
+
+	return 0;
 }
